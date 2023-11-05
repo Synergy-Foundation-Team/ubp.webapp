683aa2069f038d33e5905e98537a5488963917cf
kritsanapr <kritsana.pr.41@gmail.com>
2023-11-05 23:18:06 +0700

    ui: dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,30 @@
+import CardUI from "@/ui/CardUI";
 import React from "react";
 
 type Props = {};
 
+const card = [
+  { id: 1, text: "1" },
+  { id: 2, text: "2" },
+  { id: 3, text: "3" },
+  { id: 4, text: "4" },
+];
+
 export default function page({}: Props) {
-  return <div>Dashboard Page</div>;
+  return (
+    <div className="flex flex-wrap flex-col">
+    <div>
+          <h1 className="text-2xl md:text-4xl font-semibold my-6">
+          Dashboard
+        </h1>
+    </div>
+      <div className="flex flex-wrap flex-row gap-5 item-center">
+      {card.map((item) => (
+        <div key={item.id}>
+          <CardUI>{item.text}</CardUI>
+        </div>
+      ))}
+      </div>
+    </div>
+  );
 }
