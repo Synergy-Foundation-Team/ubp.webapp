9d4e71d19100599b05342ae2458cc21a407bbec4
akkaradet13 <akkaradet8.11.41@gmail.com>
2023-11-16 09:06:15 +0700

    update table

--- src/app/(dashboard)/menufacturial/page.tsx
+++ src/app/(dashboard)/menufacturial/page.tsx
@@ -1,20 +1,31 @@
 "use client"
+import BreadcrumbComponent from "@/components/BreadcrumbComponent";
 import BaseTable from "@/components/TableComponent/bassTable";
 import CardUI from "@/ui/CardUI";
+import InputFieldUI from "@/ui/InputFieldUI";
 import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 
 const menufacturial = () => {
   return (
-    <div>
-      <CardUI
-        fullWidth
-        classNames="bg-white m-4"
-        bodyClassName="p-6 flex flex-col items-center justify-center"
-      >
-        <div className="text-start flex justify-between w-full text-xl font-semibold text-gray-800">
-          <div>ติดตามการหมัก</div>
+    <div className="mx-6 p-4 lg:mx-12">
+      <div className="flex flex-col w-full lg:flex-row justify-between items-center mt-10">
+        <div className="flex items-center lg:w-auto">
+          <h1 className="text-4xl font-bold">ติดตามการหมัก</h1>
         </div>
-      </CardUI>
+        <div className="flex flex-col lg:flex-row gap-2 mt-4 lg:mt-0">
+          <div className="w-full lg:w-auto">
+            <InputFieldUI
+              isClearable
+              placeholder="ค้นหา"
+              startContent={<AiOutlineSearch />}
+              size="sm"
+            />
+          </div>
+        </div>
+      </div>
+      <BreadcrumbComponent />
+
       <BaseTable classNames="p-4" />
     </div>
   );
