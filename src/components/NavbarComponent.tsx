5d6136a23005f9487b5a3882b5bbb75d032021f5
kritsanapr <kritsana.pr.41@gmail.com>
2023-11-21 20:50:06 +0700

    UI:Add user profile information to NavbarComponent

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -9,15 +9,39 @@ import {
 } from "@nextui-org/react";
 import Image from "next/image";
 import SignInButton from "./SignInButton";
+import { useSession } from "next-auth/react";
 
 const NavbarUI: React.FC = () => {
+  const { data: session } = useSession();
+  const user = session?.user;
+  console.log(session);
+
   return (
     <Navbar shouldHideOnScroll isBordered maxWidth="2xl">
       <NavbarBrand>
         <p className="font-bold text-2lx text-amber-500 flex">Ubon Bio Power</p>
       </NavbarBrand>
       <NavbarContent justify="end">
-        <NavbarItem>
+        <NavbarItem className="flex flex-row gap-4 items-center">
+          {session?.user && (
+            <div className="flex flex-row max-w-sm mx-auto  items-center space-x-4">
+              <div className="shrink-0 shadow-full border-stone-800">
+                <Image
+                  className="h-12 w-12 "
+                  src="/assets/UBP_LOGO.svg"
+                  width={48}
+                  height={48}
+                  alt={`${user?.name} ${user?.email}`}
+                />
+              </div>
+              <div>
+                <div className="text-md font-medium text-black">
+                  {user?.name}
+                </div>
+                <p className="text-slate-500">{user?.email}</p>
+              </div>
+            </div>
+          )}
           <SignInButton />
         </NavbarItem>
       </NavbarContent>
